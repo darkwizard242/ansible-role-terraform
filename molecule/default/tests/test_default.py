3b5d0daca412a0cbb571de4612c571111d76f9ec
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:22:57 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,16 +5,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-TERRAFORM_BINARY_PATH = '/usr/local/bin/terraform'
-
 
 def test_terraform_binary_exists(host):
-    host.file('TERRAFORM_BINARY_PATH').exists
+    assert host.file('/usr/local/bin/terraform').exists
 
 
 def test_terraform_binary_file(host):
-    host.file('TERRAFORM_BINARY_PATH').is_file
+    assert host.file('/usr/local/bin/terraform').is_file
 
 
-def test_terraform_binary_run(host):
-    host.check_output('whereis terraform') == TERRAFORM_BINARY_PATH
+def test_terraform_binary_which(host):
+    assert host.check_output('which terraform') == '/usr/local/bin/terraform'
