9cc8c13646241b9f0e04b98b16d3c0e9093153ca
darkwizard242 <am900820@gmail.com>
2020-12-06 14:25:22 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE_BINARY = '/usr/local/bin/terraform'
+
+
 def test_terraform_binary_exists(host):
-    assert host.file('/usr/local/bin/terraform').exists
+    """
+    Tests if terraform binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_terraform_binary_file(host):
-    assert host.file('/usr/local/bin/terraform').is_file
+    """
+    Tests if terraform binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_terraform_binary_which(host):
-    assert host.check_output('which terraform') == '/usr/local/bin/terraform'
+    """
+    Tests the output to confirm terraform's binary location.
+    """
+    assert host.check_output('which terraform') == PACKAGE_BINARY
